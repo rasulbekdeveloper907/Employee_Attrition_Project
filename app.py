db65a7f1ac1436a5fd3e44c1f2354f82c267dabf
Rasulbek <rassiazzi9218@gmail.com>
2026-07-27 09:41:48 +0500

    sadasda

--- app.py
+++ app.py
@@ -2,23 +2,283 @@ import gradio as gr
 
 
 
-def square(x):
 
-    return x*x
 
-demo = gr.Interface(
-    fn=square,
-    inputs=gr.Number(
-        label="Enter Number"
-    ),
+# def word(text):
 
-    outputs=gr.Number(
-        label="Square"
-    )
+#     return len(text.split())
+
+# demo = gr.Interface(
+
+#     fn=word,
+
+#     inputs="text",
+
+#     outputs="number"
+# )
+
+# demo.launch()
+
+
+
+# def length(text):
+
+#     return len(text)
+
+# demo = gr.Interface(
+#     fn = length,
+#     inputs=gr.Textbox(),
+#     outputs="number"
+# )
+
+# demo.launch()
+
+
+
+
+
+
+
+
+# gr.Textbox(
+#     type="password"
+# )
+
+
+
+# gr.Textbox(
+#     interactive=True
+# )
+
+
+
+# gr.Textbox(
+#     interactive=False,
+#     value="Prediction Result"
+# )
+
+
+# gr.Textbox(
+#     scale=2
+# )
+
+
+
+# gr.Textbox(
+#     container=True
+# )
+
+
+
+# gr.Textbox(
+#     info="Enter Your Email"
+# )
+
+
+
+
+# def reverse(text):
+
+#     return text[::-1]
+
+
+# demo = gr.Interface(
+
+#     fn=reverse,
+
+#     inputs=gr.Textbox(
+
+#         lines=3,
+#         max_lines=10
+#     ),
+
+#     outputs="text"
+# )
+
+
+# demo.launch()
+
+
+
+
+
+
+
+
+
+
+# def reverse(text):
+
+#     return text[::-1]
+
+
+# demo = gr.Interface(
+
+#     fn=reverse,
+
+#     inputs=gr.Textbox(
+
+#         lines=5
+#     ),
+
+#     outputs="text"
+# )
+
+
+# demo.launch()
+
+
+
+
+
+
+
+
+
+# def hello(name):
+
+#     return name
+
+
+# demo = gr.Interface(
+
+#     fn=hello,
+
+#     inputs=gr.Textbox(
+
+#         value="Rasulbek"
+#     ),
+
+#     outputs= "text"
+# )
+
+
+# demo.launch()
+
+
+
+
+
+
+
+
+
+
+# def upper(text):
+
+#     return text.upper()
+
+
+# demo = gr.Interface(
+
+#     fn=upper,
+
+#     inputs=gr.Textbox(
+
+#         placeholder="Enter Text.... "
+#     ),
+
+#     outputs="text"
+# )
+
+
+# demo.launch()
+
+
+
+
+# def hello(name):
+#     return f"Hello {name}"
+
+
+# demo = gr.Interface(
+
+#     fn=hello,
+
+#     inputs=gr.Textbox(
+#             label="Your Name"
+#     ),
+
+#     outputs=gr.Textbox(
+
+#         label="Greeting"
+
+#     )
+# )
+
+
+# demo.launch()
+
+
+
+
+
+
+
+# def echo(text):
+#     return text 
+
+# demo = gr.Interface(
+#     fn=echo,
+#     inputs=gr.Textbox(),
+#     outputs=gr.Textbox()
+# )
+
+# demo.launch()
+
+
+
+
+
+
+# def greet(name):
+#     return f"Welcome {name}"
+
+
+# demo = gr.Interface(
+#     fn=greet,
+#     inputs=gr.Textbox(
+#         "Your Name"
+#     ),
+
+#     outputs=gr.Textbox(
+#         label="Result"
+#     )
+
+
+
+# )
+
+# demo.launch()
+
+
+
+
+
+
+
+
+
+
+
+
+# def square(x):
+
+#     return x*x
+
+# demo = gr.Interface(
+#     fn=square,
+#     inputs=gr.Number(
+#         label="Enter Number"
+#     ),
+
+#     outputs=gr.Number(
+#         label="Square"
+#     )
     
-)
+# )
 
-demo.launch()
+# demo.launch()
 
 
 
