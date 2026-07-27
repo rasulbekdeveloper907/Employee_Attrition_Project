7712fcc06caa5657ac07c44e671e391c7d3e5409
Rasulbek <rassiazzi9218@gmail.com>
2026-07-27 09:04:10 +0500

    afr

---
+++ app.py
@@ -0,0 +1,319 @@
+import gradio as gr 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def power(x):
+#     return x**2
+
+# demo = gr.Interface(
+#     fn= power,
+#     inputs=gr.Slider(
+#         minimum=0,
+#         maximum=100,
+#         step=1,
+#         value=25
+#     ),
+#     outputs="number"
+# )
+
+
+# demo.launch()
+
+
+
+# def choose(value):
+#     return value
+
+# demo = gr.Interface(
+#     fn= choose,
+#     inputs=gr.Radio(
+#         [
+#             "Male",
+#             "Female"            
+#         ]
+#     ),
+#     outputs="text"
+# )
+
+
+# demo.launch()
+
+
+
+
+# def select(lang):
+#     return lang
+
+# demo = gr.Interface(
+#     fn= select,
+#     inputs=gr.Dropdown(
+#         [
+#             "Python",
+#             "Java",
+#             "C++"
+#         ]
+#     ),
+#     outputs="text"
+# )
+
+
+# demo.launch()
+
+
+    
+
+
+
+
+
+
+# def hello(name):
+#     return name
+
+
+# demo = gr.Interface(
+#     fn =hello,
+#     inputs="text",
+#     outputs="text",
+# )
+
+# demo.launch()
+
+
+
+
+# demo = gr.Interface(
+#     fn =hello,
+#     inputs="number",
+#     outputs="number",
+# )
+
+
+
+
+
+
+
+
+
+# def add(a,b):
+#     return a+b
+
+
+# demo = gr.Interface(
+#     fn=add,
+#     inputs=[
+#         "number",
+#         "number"
+
+#     ],
+
+#     outputs="number"
+# )
+
+# demo.launch()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def bmi(weight, height):
+#     return weight/(height**2)
+
+
+# demo = gr.Interface(
+#     fn=bmi,
+#     inputs=[
+#         "number",
+#         "number"
+
+#     ],
+
+#     outputs="number"
+# )
+
+# demo.launch()
+
+
+
+
+
+# def hello(name):
+#     return f"Hello {name}"
+
+# demo = gr.Interface(
+#     fn = hello,
+#     inputs= "text",
+#     outputs="text"
+# )
+
+# demo.launch()
+
+
+
+
+
+
+
+
+
+# def cube(x):
+#     return x**3
+
+
+# demo = gr.Interface(
+#     fn = cube,
+#     inputs= "number",
+#     outputs="number"
+# )
+
+# demo.launch()
+
+
+
+
+
+
+
+
+
+# def square(x):
+#     return x**2
+
+
+# demo = gr.Interface(
+#     fn = square,
+#     inputs= "number",
+#     outputs="number"
+# )
+
+# demo.launch()
+
+
+
+
+
+
+
+
+
+# def count(text):
+#     return len(text)
+
+# demo = gr.Interface(
+#     fn = count,
+#     inputs= "text",
+#     outputs="number"
+# )
+
+# demo.launch()
+
+
+
+
+
+
+
+
+
+
+# def reverse(text):
+#     return text[::-1]
+
+# demo = gr.Interface(
+#     fn = reverse,
+#     inputs = "text",
+#     outputs = "text"
+# )
+
+# demo.launch()
+
+
+
+
+
+
+
+
+
+
+
+
+# def lower(text):
+#     return text.lower()
+
+# demo = gr.Interface(
+#     fn = lower,
+#     inputs="text",
+#     outputs="text"
+# )
+
+# demo.launch()
+
+
+
+
+
+
+# def upper(text):
+#     return text.upper()
+
+# demo = gr.Interface(
+#     fn = upper,
+#     inputs="text",
+#     outputs="text"
+# )
+
+# demo.launch()
+
+
+
+
+
+
+# def greet(name):
+#     return "Welcome"  + name 
+
+# app = gr.Interface(
+#     fn=greet,
+#     inputs="text",
+#     outputs="text"
+# )
+
+# app.launch()
+
+
+
+#
+# 
+#  def greet(name):
+#     return f"Hello {name}"
+
+# demo = gr.Interface(
+#     fn=greet,
+#     inputs="text",
+#     outputs="text"
+# )
+
+# demo.launch()
